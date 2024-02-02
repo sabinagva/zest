a259ccae902dc872d6791267ce6041406a29c84a
Sabina Gahramanova <sabina.ghrmnli@gmail.com>
2024-02-02 14:01:49 -0600

    added basic frontend functionality

--- src/pages/TarteCosmetics.jsx
+++ src/pages/TarteCosmetics.jsx
@@ -14,21 +14,30 @@ const TarteCosmetics = () => {
       <h1>Tarte Cosmetics</h1>
       <hr />
       <div className='tarte-makeup'>
-        {newCollection.map((item, i) => (
-          <div key={i} className='tarte-item'>
-            <Item
-              id={item.id}
-              name={item.name}
-              image={item.image}
-              new_price={item.new_price}
-              old_price={item.old_price}
-              // addToCart={addToCart}
-              className={item.image ? 'map-image' : ''}
-            />
-            <button className='btn'>Add to Cart</button>
-          </div>
-        ))}
+  {newCollection.map((item, i) => (
+    <div key={i} className='tarte-item'>
+      <Item
+        className={item.image ? 'map-image' : ''}
+        id={item.id}
+        name={item.name}
+        image={item.image}
+        new_price={item.new_price}
+        old_price={item.old_price}
+      />
+      <div className='item-details'>
+        <div className='item-name'>{item.name}</div>
+        <div className='btn-container'>
+          <button className='btn'>Add to Cart</button>
+        </div>
       </div>
+    </div>
+  ))}
+</div>
+
+
+
+
+
     </div>
   );
 };
